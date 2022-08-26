61546eaa517e7e71a3420800feaaeb6118f35e55
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-08-26 12:48:52 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
 // require packages used in the project
 const express = require('express')
-const app = express()
-const port = 3000
-
-////model
-const Restaurant = require('./models/restaurantModel')
-
+const session = require('express-session')
 // require express-handlebars here
 const exphbs = require('express-handlebars')
-
 ////bodyParser 
 const bodyParser = require
 ('body-parser')
-
 const methodOverride = require('method-override')
-
-//// mongoose connection
-require('./config/mongoose')
-////引用路由器
-const router = require('./routes')
-////express-validator
-const { body, validationResult } = require('express-validator');
-
 ////handlebars helper
 const helpers = require('handlebars-helpers')();
 
+////引用路由器
+const router = require('./routes')
+//// mongoose connection
+require('./config/mongoose')
+////express-validator
+const { body, validationResult } = require('express-validator');
 
-
+const app = express()
+const port = 3000
 
 //template engine
 app.engine('handlebars', exphbs({ 
@@ -34,23 +26,20 @@ app.engine('handlebars', exphbs({
   defaultLayout: "main"
 }))
 app.set('view engine', 'handlebars')
+app.unsubscribe(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(bodyParser.urlencoded({ extended: true }))
-
 ////static files
 app.use(express.static('public'))
-
 app.use(methodOverride('_method'))
 
-app.use(router)
+
 //routes setting
-////首頁
-
-
-
-
-
-
+app.use(router)
 
 
 app.listen(port, () => {
